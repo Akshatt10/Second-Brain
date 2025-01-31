36d8c652a285492693f3ca0e3588b636c8b1a4a8
Akshatt10 <akshattyagi1010@gmail.com>
2025-02-01 00:10:44 +0530

    Initialized the project

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// @ts-ignore // Ignore the error for now
+
+import express from "express"
+
+// npm install @types/express //-> install types for express to avoid error
+// .d.ts file is a declaration file that tells typescript what the types are for a given module
