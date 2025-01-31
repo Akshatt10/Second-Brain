88609035fa88de66b1aa350c2b43e46bb0c89177
Akshatt10 <akshattyagi1010@gmail.com>
2025-02-01 00:27:47 +0530

    dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-// @ts-ignore // Ignore the error for now
-
 import express from "express"
-
+import jsonwebtoken from "jsonwebtoken"
+import mongoose from "mongoose"
+const app = express()
 // npm install @types/express //-> install types for express to avoid error
 // .d.ts file is a declaration file that tells typescript what the types are for a given module
+
+app.post("/api/v1/signup", (req, res) => {
+  res.send("Hello World")
+})
+
+app.post("/api/v1/signin", (req, res) => { })
+
+app.post("/api/v1/content", (req, res) => { })  
+
+app.get("/api/v1/content", (req, res) => { })
+
+app.delete("/api/v1/content", (req, res) => { })    
+
+app.post("/api/v1/brain/share", (req, res) => { })
+
+app.get("/api/v1/brain/:shareLink", (req, res) => { })
